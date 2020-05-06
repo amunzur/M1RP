caadd6c70a46f2f8efe28fd6c2e2322d91c30cf1
Andrew Murtha <andy.murtha@gmail.com>
2020-05-06 16:29:36 -0700

    Update create_blacklists.py

--- dev/mutation_blacklist_creation/create_blacklists.py
+++ dev/mutation_blacklist_creation/create_blacklists.py
@@ -8,7 +8,7 @@ Created on Wed May  6 11:31:49 2020
 import pandas as pd
 import numpy as np
 
-cohort = 'M1RP'
+cohort = 'M1B'
 
 # =============================================================================
 # Import data
@@ -81,6 +81,21 @@ mut_counts = mut_counts[['Sample ID']].rename(columns = {'Sample ID':'Count'})
 mut_counts = mut_counts[mut_counts['Count'] == 1]
 blacklist = blacklist[blacklist.index.isin(mut_counts.index)]
 
+# =============================================================================
+# Check the cosmic score
+# =============================================================================
+
+blacklist['NOTES'] = blacklist['NOTES'].str.split('\. ').str[1]
+for index, row in blacklist.iterrows():
+    if 'COSMIC' in row['NOTES']:
+        cosmic = int(row['NOTES'].split('COSMIC:')[1].split('.')[0])
+        if cosmic > 10:
+            blacklist.drop(index)
+            
+blacklist = blacklist[~blacklist['Sample ID'].str.contains('cfDNA')]
+
+blacklist.to_csv('C:/Users/amurtha/Dropbox/Ghent M1 2019/sandbox/mutations/blacklists/%s_blacklist_full.tsv' % cohort, sep = '\t', header = None, index = None)
+
 blacklist = blacklist.reset_index()[['CHROM','POSITION','REF','ALT']]
 
-blacklist.to_csv('C:/Users/amurtha/Dropbox/Ghent M1 2019/sandbox/mutations/blacklists/%s_blacklist.tsv' % cohort, sep = '\t', header = None, index = None)
+blacklist.to_csv('C:/Users/amurtha/Dropbox/Ghent M1 2019/sandbox/mutations/blacklists/%s_blacklist_upload.tsv' % cohort, sep = '\t', header = None, index = None)
