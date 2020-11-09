b4b90190812faf5d1bb352c10cc0d7e4713fd96b
swsng <59742703+swsng@users.noreply.github.com>
2020-11-09 14:48:03 -0800

    Update tc_comparisons.py

--- dev/targeted_vs_wes_concordance/tc_comparisons.py
+++ dev/targeted_vs_wes_concordance/tc_comparisons.py
@@ -14,6 +14,9 @@ import seaborn as sns
 from scipy import stats
 
 
+
+plt.rcParams.update({'font.size': 12})
+
 # =============================================================================
 # TC from google sheets
 # =============================================================================
@@ -41,7 +44,7 @@ ax1.set_title('M1RP and M1B TC estimates \n Final tNGS_TC')
 sns.stripplot(x="Cohort",y="Final tNGS_TC",data=tc,palette="Set2",ax=ax1,zorder=0,dodge=True, size = 3)
 sns.boxplot(x="Cohort",y="Final tNGS_TC",data=tc, whis=np.inf,ax=ax1,color='grey',boxprops=dict(alpha=.1),zorder=10,dodge=True, width=0.5, linewidth = 1)
 
-
+ax1.set_ylabel('Tumour Content %')
 ax1.set_ylim(0,100)
 ax1.spines['right'].set_visible(False)
 ax1.spines['top'].set_visible(False)
@@ -51,11 +54,12 @@ m1b = tc[tc['Cohort'] == 'M1B']['Final tNGS_TC']
 m1rp = tc[tc['Cohort'] == 'M1RP']['Final tNGS_TC']
 tstat, pvalue = stats.ttest_ind(m1rp, m1b)
 
+
 # MannwhitneyU
 stat, p = stats.mannwhitneyu(m1b, m1rp)
 
 
-fig.savefig('C:\\Users\\Sarah\\Desktop\\M1RPvsM1B_TC_boxplot.pdf', bbox_inches='tight')
+fig.savefig('C:\\Users\\Sarah\\Desktop\\M1RPvsM1B_TC_boxplot.png', bbox_inches='tight', dpi = 600)
 
 # =============================================================================
 # WES vs Targeted 
@@ -74,7 +78,6 @@ wes.columns = ['TC']
 target['Type'] = 'Targeted'
 wes['Type'] = 'WES'
 
-# Paired T test or mann whitney U test
 tstat, pvalue = stats.ttest_rel(target['TC'], wes['TC'])
 stat, p = stats.mannwhitneyu(target['TC'], wes['TC'])
 
@@ -89,13 +92,14 @@ ax1.set_title('Targeted and WES TC estimates \n M1RP')
 sns.stripplot(x="Type",y="TC",data=tc,palette="Set2",ax=ax1,zorder=0,dodge=True, size = 3)
 sns.boxplot(x="Type",y="TC",data=tc, whis=np.inf,ax=ax1,color='grey',boxprops=dict(alpha=.1),zorder=10,dodge=True, width=0.5, linewidth = 1)
 
-
+ax1.set_ylabel('Tumour Content %')
 ax1.set_ylim(0,100)
 ax1.spines['right'].set_visible(False)
 ax1.spines['top'].set_visible(False)
 
 
-fig.savefig('C:\\Users\\Sarah\\Desktop\\Targeted_WES_compare_TC_boxplot.pdf', bbox_inches='tight')
+
+fig.savefig('C:\\Users\\Sarah\\Desktop\\Targeted_WES_compare_TC_boxplot.png', dpi=600, bbox_inches='tight')
 
 
 
